573bbd7a93222a96c258b64c89211fef9118abb5
v-rex401 <vashtipillay104@gmail.com>
2025-04-13 15:55:43 +0200

    Addded a input number feature

--- main.cpp
+++ main.cpp
@@ -2,5 +2,9 @@
 
 int main() {
     std::cout << "Hello World"; 
+    std::cout << "Please enter a value: "; 
+    int value; 
+    std::cin >> value; 
+    std::cout << "This is your value: " << value; 
     return 0; 
 }
